7d99cf082b3f71b50042d223a730deaa66b0e03c
Daniel-RG <21danielrondong@gmail.com>
2023-08-28 11:56:22 +0200

    change DR photo and CV

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { RiDoubleQuotesL } from "react-icons/ri";
-import danielPhoto from "../images/danielRG_photo_noBG.png";
+import danielPhoto from "../images/danielRG_photo_blueBG.png";
 import TestimonialsModal from "../components/TestimonialsModal";
 import Carousel from "../components/Carousel";
 
